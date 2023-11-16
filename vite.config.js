c65204852e0d35129bcd94e206b4f7e28ad3689d
M3LKY <robinjut10000@gmail.com>
2023-11-16 02:23:58 -0500

    change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_CONNECTO_API,
-      }
-    }
-  },
-});
+})
