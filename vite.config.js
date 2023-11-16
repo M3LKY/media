08c57376cc4aa1e7a00afa01f8eb4e67658d9c4d
M3LKY <robinjut10000@gmail.com>
2023-11-16 02:09:14 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-<<<<<<< HEAD
       "/api": {
         target: import.meta.env.VITE_CONNECTO_API,
-=======
-      '/api': {
-        target: 'https://connectserver-c64m.onrender.com',
->>>>>>> 29fb411f6aa1dd26c31b96acc58dc34df715c4e0
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+      },
+    },
   },
-  build: {
-    rollupOptions: {
-      external: ['https://connectserver-c64m.onrender.com']
-    }
-  }
 });
