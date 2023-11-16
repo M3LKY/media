54bb41ca648289afc04aa0c5f63cf11ee7e4754a
M3LKY <105396866+M3LKY@users.noreply.github.com>
2023-11-16 02:49:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://connectserver-c64m.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['https://connectserver-c64m.onrender.com']
-    }
-  }
+  
 });
