0315cf24563cbbc094db56efdb61c195e6f052b8
M3LKY <robinjut10000@gmail.com>
2023-11-16 02:29:17 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_CONNECTO_API,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['https://connectserver-c64m.onrender.com']
+    }
+  }
+});
